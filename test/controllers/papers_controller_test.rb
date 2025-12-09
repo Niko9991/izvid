bf696bb122a23f7d3e85484996403d3b44a975aa
Niko Osolnik <prvi@ocene.si>
2025-12-09 04:27:40 +0000

    fixtures modified loaded

--- test/controllers/papers_controller_test.rb
+++ test/controllers/papers_controller_test.rb
@@ -17,7 +17,7 @@ class PapersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create paper" do
     assert_difference("Paper.count") do
-      post papers_url, params: { paper: { article: @paper.article, certificate: @paper.certificate, certificate_count: @paper.certificate_count, comment: @paper.comment, group: @paper.group, labtest: @paper.labtest, labtest_count: @paper.labtest_count, location_certificate: @paper.location_certificate, location_labtest: @paper.location_labtest, supplier_id: @paper.supplier_id, year: @paper.year } }
+      post papers_url, params: { paper: { article: @paper.article, certificate: @paper.certificate, comment: @paper.comment, group: @paper.group, labtest: @paper.labtest, location_certificate: @paper.location_certificate, location_labtest: @paper.location_labtest, supplier_id: @paper.supplier_id, year: @paper.year } }
     end
 
     assert_redirected_to paper_url(Paper.last)
@@ -34,15 +34,15 @@ class PapersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update paper" do
-    patch paper_url(@paper), params: { paper: { article: @paper.article, certificate: @paper.certificate, certificate_count: @paper.certificate_count, comment: @paper.comment, group: @paper.group, labtest: @paper.labtest, labtest_count: @paper.labtest_count, location_certificate: @paper.location_certificate, location_labtest: @paper.location_labtest, supplier_id: @paper.supplier_id, year: @paper.year } }
+    patch paper_url(@paper), params: { paper: { article: @paper.article, certificate: @paper.certificate, comment: @paper.comment, group: @paper.group, labtest: @paper.labtest, location_certificate: @paper.location_certificate, location_labtest: @paper.location_labtest, supplier_id: @paper.supplier_id, year: @paper.year } }
     assert_redirected_to paper_url(@paper)
   end
 
-  test "should destroy paper" do
-    assert_difference("Paper.count", -1) do
-      delete paper_url(@paper)
-    end
-
-    assert_redirected_to papers_url
+ test "should destroy paper" do
+  assert_difference("Paper.count", -1) do
+    delete supplier_paper_url(@paper.supplier, @paper)
   end
+
+  assert_redirected_to supplier_papers_url(@paper.supplier)
+end
 end
